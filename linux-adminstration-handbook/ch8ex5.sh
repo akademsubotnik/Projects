e8cc31cae7437112d9ca659f60d6e2334d9a6251
Greg Koval <44036625+gregg00@users.noreply.github.com>
2020-11-02 21:57:53 -0500

    Create ch8ex5.sh

---
+++ linux-adminstration-handbook/ch8ex5.sh
@@ -0,0 +1,5 @@
+Using the man pages for the du,sort, and head commands as references, write a script that determines which 10 home directories are the largest on the system. Schedule the script to run every Monday night at 12:00 a.m.and have it mail its output to you. Hint: you’ll want to use a reverse nu-meric sort. (Requires root access.)
+
+du - estimate file space usage
+sort - sort lines of text files
+head - output the first part of files
