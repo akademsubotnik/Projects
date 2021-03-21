d471302c508d0fb06e503cbc1f4afa3d1a6624c7
Greg Koval <44036625+gregg00@users.noreply.github.com>
2021-03-21 13:36:00 +0200

    Update eXf.sh

--- bash/Extract Files/eXf.sh
+++ bash/Extract Files/eXf.sh
@@ -12,12 +12,12 @@ elif [ "${1: -3}" == ".gz" ]; then
 elif [ "${1: -4}" == ".bz2" ]; then
 	echo ".bz2 file"
 	bzip2 -d $1
-elif [ "${1: -4}" == ".rar" ]; then
-	echo ".rar file"
-	unrar -e
 elif [ "${1: -3}" == ".7z" ]; then
 	echo ".7z file"
 	7zr e $1
+elif [ "${1: -4}" == ".rar" ]; then
+	echo ".rar file"
+	unrar -e
 elif [ "${1: -4}" == ".tgz" ]; then
 	echo ".tgz file"
 else 
